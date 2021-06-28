db8b20c377be9ca3f00b81896ef48399b50fdb45
Douglas Oliveira <douglasoliveira163425@gmail.com>
2021-06-27 21:37:54 -0400

    add filtro de vendedores e cidade

--- app.py
+++ app.py
@@ -26,13 +26,6 @@ fig_comissao_vend.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
 df_prods.rename(columns={'safra': 'SAFRA'}, inplace=True)
 fig_prods_safra = px.line(df_prods, x='SAFRA', y=df_prods.columns, title='Venda de Produtos')
 
-# Localização dos Clientes
-fig_map_clientes = px.scatter_mapbox(df_map_clientes, lat="lat", lon="lon", hover_name="Cidade", hover_data=["Estado","Populacao","Cliente"],
-                        color_discrete_sequence=["fuchsia"], zoom=5, height=500)
-fig_map_clientes.update_layout(mapbox_style="open-street-map")
-fig_map_clientes.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig_map_clientes.update_layout(title="Localização dos Clientes")
-
 app = dash.Dash()
 app.layout = html.Div(
     [
@@ -50,18 +43,48 @@ app.layout = html.Div(
             id='fig_receita_despesa'
         ),
         html.Br(),
+        dcc.Dropdown(
+            id='dd_comissao_vend',
+            options=[
+                {'label': 'Wellen', 'value': 'Wellen'},
+                {'label': 'Douglas', 'value': 'Douglas'},
+                {'label': 'Adao', 'value': 'Adao'},
+                {'label': 'Janice', 'value': 'Janice'},
+                {'label': 'Gabriel', 'value': 'Gabriel'}
+            ],
+            value='Douglas',
+            placeholder='Informe um Vendedor',
+            multi=False
+        ),
+        dcc.Graph(
+            id='fig_comissao_vendedores'
+        ),
         dcc.Graph(figure=fig_comissao_vend),
         html.Br(),
         dcc.Graph(figure=fig_prods_safra),
         html.Br(),
-        dcc.Graph(figure=fig_map_clientes),
-        html.Br(),
         dcc.Dropdown(
             id='dd_mapa_clientes',
             options=[
-                {'label': 'Itiquira', 'value': 'Itiquira'}
+                {'label': 'Itiquira', 'value': 'Itiquira'},
+                {'label': 'Rondonopolis', 'value': 'Rondonopolis'},
+                {'label': 'Pedra Preta', 'value': 'Pedra Preta'},
+                {'label': 'Juscimeira', 'value': 'Juscimeira'},
+                {'label': 'Alto Garças', 'value': 'Alto Garças'},
+                {'label': 'Itiquira', 'value': 'Itiquira'},
+                {'label': 'Cuiaba', 'value': 'Cuiaba'},
+                {'label': 'Caceres', 'value': 'Caceres'},
+                {'label': 'Lucas do Rio Verde', 'value': 'Lucas do Rio Verde'},
+                {'label': 'Sorriso', 'value': 'Sorriso'},
+                {'label': 'Sinop', 'value': 'Sinop'},
+                {'label': 'Brasnorte', 'value': 'Brasnorte'},
+                {'label': 'Juina', 'value': 'Juina'},
+                {'label': 'Castanheira', 'value': 'Castanheira'},
+                {'label': 'Tangara da Serra', 'value': 'Tangara da Serra'},
+                {'label': 'Sonora', 'value': 'Sonora'},
+                {'label': 'Mineiros', 'value': 'Mineiros'}
             ],
-            value='Itiquira',
+            value='Rondonopolis', # Aqui poderia ser passado uma lista de cidades
             placeholder='Seleciona um Estado',
             multi=False
         ),
@@ -82,14 +105,16 @@ def graf_receita_desp(dd_receita_despesa):
     return px.line(df_rec_desp, x='PERIODO', y=dd_receita_despesa, title='Receitas e Despesas')
 
 
-# Filtro de Clientes
+# Filtro de Localição de Clientes
 @app.callback(
     Output(component_id='fig_mapa_clientes', component_property='figure'),
     [Input(component_id='dd_mapa_clientes', component_property='value')]
 )
 def graf_mapa_clientes(dd_mapa_clientes):
-    df_mapa_clientes = df[df['Cidade' == dd_mapa_clientes]]
-    fig_mapa_clientes = px.scatter_mapbox(df_mapa_clientes, lat="lat", lon="lon", hover_name="Cidade",
+    dfc = df.filter(items=['Cidade', 'Estado', 'Populacao', 'Cliente', 'lat', 'lon'])
+    dfc2 = dfc.loc[dfc.Cidade == dd_mapa_clientes]
+    #  dfc2 = df.loc[df['Cidade'].isin(dd_mapa_clientes)]  # Fazer assim para quando for uma lista de cidades
+    fig_mapa_clientes = px.scatter_mapbox(dfc2, lat="lat", lon="lon", hover_name="Cidade",
                                          hover_data=["Estado", "Populacao", "Cliente"],
                                          color_discrete_sequence=["fuchsia"], zoom=5, height=500)
     fig_mapa_clientes.update_layout(mapbox_style="open-street-map")
@@ -97,6 +122,21 @@ def graf_mapa_clientes(dd_mapa_clientes):
     fig_mapa_clientes.update_layout(title="Localização dos Clientes")
     return fig_mapa_clientes
 
+#Filtro de Vendedores - Comissao
+@app.callback(
+    Output(component_id='fig_comissao_vendedores', component_property='figure'),
+    [Input(component_id='dd_comissao_vend', component_property='value')]
+)
+def graf_mapa_vendedores(dd_comissao_vend):
+    df_comissao_vend.rename(columns={'valor': 'VALOR', 'vendedor': 'VENDEDOR'}, inplace=True)
+
+    dfc = df_comissao_vend.filter(items=['VENDEDOR', 'VALOR', 'mes', 'produto'])
+    dfc2 = dfc.loc[dfc.VENDEDOR == dd_comissao_vend]
+
+    fig_comissao_vendedores = px.bar(dfc2, y='VALOR', x='VENDEDOR', text='VALOR', title='Comissão de Vendedores')
+    fig_comissao_vendedores.update_traces(texttemplate='%{text:.2s}', textposition='outside')
+    fig_comissao_vendedores.update_layout(uniformtext_minsize=8, uniformtext_mode='hide')
+    return fig_comissao_vendedores
 
 app.run_server(debug=True, use_reloader=True)
 
